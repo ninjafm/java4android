8e459232b91f8cad2c8f4780c46daccb78e42cba
ninjafm <neretinev@yandex.ru>
2021-03-13 16:52:44 +0300

    Все задания выполнил. Что означает "обработать возможные исключения"? Как еще можно переписать код для прямоугольного массива? Не пойму как в таком случае записывать arr.length?

---
+++ src/main/java/Lesson9/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package Lesson9;
+
+public class MyArrayDataException extends Exception{
+    public MyArrayDataException() {
+        super("В яйчейке лежит текст или буква");
+    }
+}
---
+++ src/main/java/Lesson9/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package Lesson9;
+
+public class MyArraySizeException extends Exception{
+    public MyArraySizeException() {
+        super("Массив не 4x4!");
+    }
+}
